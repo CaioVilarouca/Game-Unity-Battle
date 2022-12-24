777eb894df038b6b7ee480171f206c2cc4342711
Desenvolvedor <82101110+CaioVilarouca@users.noreply.github.com>
2022-12-24 15:17:26 -0300

    Transição de tela

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Menu: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void PlayerBattle(string Battle) { 
+		SceneManager.LoadScene("Battle");
+	}
+	
+	public void BackMenu(string Menu){
+		SceneManager.LoadScene("Menu");
+	}
+
+	public void ExitGame() {
+		Application.Quit();
+	}
+}
